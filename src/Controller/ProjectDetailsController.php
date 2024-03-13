f04385a2f3bec4a0cd7d4b0881dfd19367e1c116
Łukasz Bacik <mail@luka.sh>
2024-03-13 12:01:41 +0100

    Update project details page(s)

--- src/Controller/ProjectDetailsController.php
+++ src/Controller/ProjectDetailsController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use App\Service\ProjectService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,8 +18,8 @@ class ProjectDetailsController extends AbstractController
     ) {
     }
 
-    #[Route('/projects/{id}', name: 'app_project_details')]
-    public function index(string $id): Response
+    #[Route('/projects/{id}/{tab}', name: 'app_project_details', methods: ['GET'])]
+    public function index(string $id, string|null $tab = 'about'): Response
     {
         $project = $this->projectService->getProject($id);
 
@@ -25,8 +27,29 @@ class ProjectDetailsController extends AbstractController
             throw $this->createNotFoundException('The project does not exist');
         }
 
+        if ($tab === 'contact') {
+            $contactForm = $this->createForm(ContactType::class);
+        }
+
         return $this->render('project_details/index.html.twig', [
+            'projectId' => $id,
+            'tab' => $tab,
             'project' => $project,
+            'contactForm' => $contactForm ?? null,
         ]);
+    }
+
+    #[Route('/projects/{id}/contact', name: 'app_project_contact', methods: ['POST'])]
+    public function contact(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Your message has been sent');
+            return new Response(null, 204);
+        }
+
+        return new Response(null, 400);
     }
 }
