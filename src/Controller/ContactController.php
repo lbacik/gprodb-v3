4476600ccc77fa0fc0cd7bd8f8381816bd7a4dfc
Łukasz Bacik <mail@luka.sh>
2024-03-14 08:36:50 +0100

    Add home, about and contact pages

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $contact = $form->getData();
+            // do something with the contact
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
