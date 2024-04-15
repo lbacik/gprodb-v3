df8d70eff81497ddd0431a1e880b3f653d2726c1
Łukasz Bacik <mail@luka.sh>
2024-04-15 16:24:54 +0200

    Update login

--- src/Infrastructure/JsonHub/Security/JsonHubAuthenticator.php
+++ src/Infrastructure/JsonHub/Security/JsonHubAuthenticator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Infrastructure\JsonHub\Security;
 
 use App\Entity\User;
@@ -7,32 +9,40 @@ use App\Infrastructure\JsonHub\Service\JSONHubService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class JsonHubAuthenticator extends AbstractAuthenticator
+class JsonHubAuthenticator extends AbstractLoginFormAuthenticator
 {
+    use TargetPathTrait;
+
     public function __construct(
         private readonly JSONHubService $jsonHubClient,
         private readonly RouterInterface $router,
+        private readonly UrlGeneratorInterface $urlGenerator,
     ) {
     }
 
-    public function supports(Request $request): ?bool
+    public function supports(Request $request): bool
     {
         return $request->getPathInfo() === '/login' && $request->isMethod('POST');
     }
 
     public function authenticate(Request $request): Passport
     {
-        $email = $request->request->get('email');
+        $email = $request->request->get('email', '');
+        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
+
         $password = $request->request->get('password');
 
         return new Passport(
@@ -61,15 +71,19 @@ class JsonHubAuthenticator extends AbstractAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        return new RedirectResponse($this->router->generate('app_project_list'));
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
-    {
-        $request->getSession()->set(SecurityRequestAttributes::AUTHENTICATION_ERROR, $exception);
-
-        return new RedirectResponse($this->router->generate('app_login'));
-    }
+//    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+//    {
+//        $request->getSession()->set(SecurityRequestAttributes::AUTHENTICATION_ERROR, $exception);
+//
+//        return new RedirectResponse($this->router->generate('app_login'));
+//    }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
 //    {
@@ -81,4 +95,8 @@ class JsonHubAuthenticator extends AbstractAuthenticator
 //         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
 //         */
 //    }
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate('app_login');
+    }
 }
