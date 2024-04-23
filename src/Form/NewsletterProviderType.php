7eb4070410a012221fadb107df5ee98c1b0221d4
Łukasz Bacik <mail@luka.sh>
2024-04-23 13:53:00 +0200

    Update mailing support

--- src/Form/NewsletterProviderType.php
+++ src/Form/NewsletterProviderType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\MailingProvider;
+use App\Type\MailingProvider as MailingProviderEnum;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,19 +15,29 @@ class NewsletterProviderType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('provider', ChoiceType::class, [
-                'choices' => [
-                    'Generic' => 'generic',
-                    'MailingR' => 'mailingr',
-                ],
+            ->add('name', ChoiceType::class, [
+                'choices' => array_reduce(
+                    MailingProviderEnum::values(),
+                    fn (array $choices, string $name) => array_merge($choices, [$name => $name]),
+                    [],
+                ),
+                'label' => 'Provider',
             ])
         ;
+
+        $builder->get('name')
+            ->addModelTransformer(
+                new CallbackTransformer(
+                    fn (MailingProviderEnum $name) => $name->value,
+                    fn (string $name) => MailingProviderEnum::from($name),
+                )
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => MailingProvider::class,
         ]);
     }
 }
