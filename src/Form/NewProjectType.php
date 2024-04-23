acb7d014db3401aa2adb0fb691c4b704a43c7219
Łukasz Bacik <mail@luka.sh>
2024-04-23 14:01:19 +0200

    Draft - new field for new project form

--- src/Form/NewProjectType.php
+++ src/Form/NewProjectType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,6 +20,17 @@ class NewProjectType extends AbstractType
                     'placeholder' => 'Enter project name',
                 ],
             ])
+//            ->add('customSlug', CheckboxType::class, [
+//                'label' => 'Custom slug',
+//                'required' => false,
+//            ])
+//            ->add('slug', TextType::class, [
+//                'label' => false,
+//                'required' => false,
+//                'attr' => [
+//                    'readonly' => true,
+//                ],
+//            ])
         ;
     }
 
