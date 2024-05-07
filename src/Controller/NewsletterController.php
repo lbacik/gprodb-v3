67150e8a952a88b6c060291b525f774eedd8a891
Łukasz Bacik <mail@luka.sh>
2024-05-07 14:10:24 +0200

    Update newsletter

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -9,23 +9,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class NewsletterController extends AbstractController
 {
     #[Route('/subscribe', name: 'app_subscribe', methods: ['POST'])]
-    public function subscribe(Request $request, NewsletterService $newsletterService): Response
-    {
+    public function subscribe(
+        Request $request,
+        NewsletterService $newsletterService,
+        TranslatorInterface $translator,
+    ): Response {
         $csrfToken = $request->getPayload()->get('csrf_token');
 
         if (!$this->isCsrfTokenValid('newsletterSubscribe', $csrfToken)) {
-            throw new \RuntimeException('CSRF token is invalid');
+            $this->addFlash('error', $translator->trans('newsletter.error'));
+
+            return $this->redirectToRoute($request->get('_route'));
         }
 
         $email = $request->request->get('email');
         $newsletterService->subscribe($email);
 
-        $this->addFlash('success', 'You have been successfully subscribed to our newsletter');
+        $this->addFlash('success', $translator->trans('newsletter.success'));
 
-        return new Response(null, Response::HTTP_NO_CONTENT);
+        return $this->redirectToRoute($request->get('_route'));
     }
 }
